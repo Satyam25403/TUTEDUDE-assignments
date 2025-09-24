5d086a769e8b77eb111970c3cebc3687df5abbf5
Satyam250304 <228w1a05i5@vrsec.ac.in>
2025-09-24 20:13:27 +0530

    3rd question

---
+++ Terraform assignment/03question/variables.tf
@@ -0,0 +1,7 @@
+variable "flask_image" {
+  default = "241592945434.dkr.ecr.eu-north-1.amazonaws.com/flask-backend:latest"
+}
+
+variable "express_image" {
+  default = "241592945434.dkr.ecr.eu-north-1.amazonaws.com/express-frontend:latest"
+}
